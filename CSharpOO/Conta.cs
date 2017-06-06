18d04e090f61015236fc68d325b0e7ab38af8d97
Carlos H. M <carlosmarian@gmail.com>
2017-06-05 22:48:15 -0300

    herança 1b

---
+++ CSharpOO/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpOO
+{
+    public class Conta
+    {
+        int agencia;
+
+        int numeroConta;
+        decimal saldo;
+
+        public int Agencia
+        {
+            get { return agencia; }
+            set
+            {
+                if (value > 0)
+                    agencia = value;
+            }
+        }
+
+        public int NumeroConta { get => numeroConta; set => numeroConta = value; }
+        public decimal Saldo { get => saldo; set => saldo = value; }
+
+
+
+        /**
+         * decrementa o saldo.
+         * */
+        void Sacar(decimal valor)
+        {
+            Saldo -= valor;
+        }
+
+        /**
+         * INcrementa o saldo*/
+        void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+    }
+}
